d5faa6171c729a3c4520d307ba57b45eb6bf5636
medeiroslucass <lcsmedeiroscosta@gmail.com>
2023-07-13 23:09:25 -0300

    teste
    Signed-off-by: medeiroslucass <lcsmedeiroscosta@gmail.com>

--- math_test.py
+++ math_test.py
@@ -1,9 +1,10 @@
-from math import soma
 import unittest
+from math_1 import soma
 
 class TestSoma(unittest.TestCase):
     def test_soma(self):
         total = soma(15, 10)
+        print(f"TOTAL: {total}")
         self.assertEqual(total, 30, "Resultado da soma invalido")
 
 if __name__ == "__main__":
